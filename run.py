fe3f17ca3d96a099491116a729675db05366cc49
Sean Tilson <seantilson@gmail.com>
2024-04-23 14:12:39 +0000

    in progress: wokring on loop for completing a task, add_to_task finished.

--- run.py
+++ run.py
@@ -10,13 +10,13 @@ class GreatOldOne:
     """
     Creates Great Old One enemy.
     """
-    def __init__(self, name:str, health:int, challenges:tuple, attack:str) -> None:
+    def __init__(self, name:str, health:int, tasks:tuple, attack:str) -> None:
         self.name = name
         if health <=0:
             raise ValueError(f"{health} is not a valid starting health value.")
         self.starting_health = health
         self.health = health
-        self.challenges = challenges
+        self.tasks = tasks
         self.attack = attack # this should be replaced by something later
     
     # Current win condition
@@ -33,8 +33,8 @@ class GreatOldOne:
         if self.health < 0:
             self.health = 0
     
-    # should this be check challenges?
-    def check_challenge(self, challenge, die_sequence):
+    # should this be check tasks?
+    def check_task(self, task, die_sequence):
         pass
 
 class Character:
@@ -51,8 +51,8 @@ class Character:
             self.items = items
         else:
             self.items = []
-        self.dice = 6
-        self.current_roll = ['' for _ in range(6)]
+        self.dice = 6 # this val will be modified, reset it at the beginning of each turn or at the end of each turn.
+        self.current_roll = ['' for _ in range(self.dice)]
 
     # Current loss condition
     @property
@@ -72,20 +72,20 @@ class Character:
         self.current_roll = [green_die.roll() for _ in range(self.dice)]
 
     # should there be an undo feature?
-    def attempt_challenge(self, index:int, challenge) -> None:
+    def attempt_task(self, index:int, task) -> None:
         die = self.current_roll.pop(index)
         try:
-            challenge.add_die(die)
+            task.add_die(die)
         except ValueError as e:
             print(e)
             self.current_roll.append(die)
-            print("Select a different challenge.")
+            print("Select a different die.")
     
 # needs validation
 # needs overflow check method maybe?
-class Challenge:
+class task:
     """
-    Create challenge object. The pattern is what is necessary to succeed at a challenge. The reward is what happens when you succeed, the penalty is what happens when you fail.
+    Create task object. The pattern is what is necessary to succeed at a task. The reward is what happens when you succeed, the penalty is what happens when you fail.
     """
     # reward/penalty are currently strings, but should be changed to something else.
     def __init__(self, name: str, pattern: dict, reward: str, penalty: str) -> None:
@@ -103,17 +103,29 @@ class Challenge:
         else:
             # should this be an error
             raise ValueError(f"""{die_face} is not 
-            contained in the pattern for this challenge.""")
+            contained in the pattern for this task.""")
         # what should I do about exceeding the maximum error?
         # maybe that is a separate method
-    
+
+    def __contains__(self, current_roll) -> bool:
+        symbols = {die.split(' x ') for die in current_roll}
+        for symbol in symbols:
+            if symbol in self.pattern.keys():
+                return True
+        return False
+
+    @property
+    def remaining(self) -> dict:
+        return {key:self.pattern[key]-self.slots[key] for key in self.pattern.keys()}
+
+    # add __str__ method for task
     @property
     def complete(self):
         for key in self.pattern.keys():
             if self.pattern[key]> self.slots[key]:
-                print(f"Challenge is not yet complete, you still need {self.pattern[key] - self.slots[key]} {key}'s.")
+                print(f"The task is not yet complete, you still need {self.pattern[key] - self.slots[key]} {key}'s.")
                 return false
-        print(f'You have completed the Challenge: {self.name}!')
+        print(f'You have completed the task: {self.name}!')
         print(f"You get a {self.reward}.")
         return True
 
@@ -121,7 +133,7 @@ class Die:
     SYMBOLS = {'1 x Investigate','2 x Investigate','3 x Investigate','4 x Investigate','1 x Scroll','2 x Scroll', '1 x Skull', '1 x Tentacles'}
     def __init__(self, faces:tuple)-> None:
         for face in faces:
-            if face in self.SYMBOLS:
+            if face in Die.SYMBOLS:
                 continue
             else:
                 raise ValueError(f"{face} is not a valid symbol. We can not create this die.")
@@ -134,14 +146,43 @@ class Die:
 
 green_die = Die(('1 x Investigate','2 x Investigate','3 x Investigate','1 x Scroll', '1 x Skull', '1 x Tentacles'))
 
+def add_to_task(current_roll, task):
+    """
+    Assigns dice from current roll to fixed task as long as it is possible.
+    Returns task. Should it also return the remaining dice? Yes.
+    """
+    # current roll is fixed
+    if not current_roll in task:
+        print("None of the symbols of this role are in this task.")
+        return current_roll, task
+    while not task.complete and current_roll in task:
+        print(current_roll)
+        print(task.remaining)
+        index = get_die_choice(len(current_roll))
+        task.add_die(current_roll.pop(index-1))
+    return current_roll, task
+
+def get_die_choice(num_dice: int): #return value for this is a bit complex
+    # what about when num_dice = 0 or 1?
+    index = ""
+    valid_input = [str(num+1) for num in range(num_dice)]
+    valid_input.append("pass")
+    while index not in valid_input:
+        index = input(f"""Please input numbers 1-{len(character.current_roll)} to select which die to assign to the task.\n
+                    If none of your dice will work, enter pass.\n""")
+    if index.lower() == "pass":
+        return -1
+    else:
+        return int(index)
+        
 
 def create_generic():
     """
     This function creates basic instances of the above classes for the purpose of development.
     """
-    basic_challenge1 = Challenge(name='basic_attack',pattern={'Investigate':2, 'Skull':1}, reward="+1 damage", penalty="-1 health")
-    basic_challenge2 = Challenge(name='basic_heal',pattern={'Investigate':1, 'Scroll':2}, reward="+2 health", penalty="-1 health")
-    basic_old_one = GreatOldOne("basic old one", 10, (basic_challenge1, basic_challenge2), "+2 damage")
+    basic_task1 = task(name='basic_attack',pattern={'Investigate':2, 'Skull':1}, reward="+1 damage", penalty="-1 health")
+    basic_task2 = task(name='basic_heal',pattern={'Investigate':1, 'Scroll':2}, reward="+2 health", penalty="-1 health")
+    basic_old_one = GreatOldOne("basic old one", 10, (basic_task1, basic_task2), "+2 damage")
     basic_character = Character("joe shmoe",6)
     return basic_old_one, basic_character
 
@@ -157,5 +198,5 @@ def start_game():
 
 # current_progress
 old_one, joe = create_generic()
-c1, c2 = old_one.challenges
+c1, c2 = old_one.tasks
 joe.roll_dice()
