08358839ae48589ad2a9f3f58862a065daaca061
Sean Tilson <seantilson@gmail.com>
2024-04-24 10:47:12 +0000

    feat: add DicePool object and methods and refactor appropriately
    in progress: add attempt_task_card function to enlarge gameplay loop

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ class Character:
         else:
             self.items = []
         # self.dice = 6 # this val will be modified, reset it at the beginning of each turn or at the end of each turn.
-        self.dice_pool = [Die.green() for _ in range(6)]
+        self.dice_pool = DicePool()
 
     def __str__(self):
         return f"{self.name} has {self.health} left."
@@ -74,13 +74,7 @@ class Character:
             self.health = 0
     
     def roll_dice(self) -> None:
-        for die in self.dice_pool:
-            die.roll()
-
-    # this now seems irrelevant with the __repr__ method of Die now in place.
-    @property    
-    def current_roll(self) -> list:
-        return [die.face for die in self.dice_pool]
+        self.dice_pool.roll()
     
     @property
     def dice(self) -> int:
@@ -92,7 +86,8 @@ class Character:
         # print(f"Advancing clock. It is now {clock.time}")
         if self.alive:
             # This resets the dice pool and the face of each die to initial face
-            self.dice_pool = [Die.green() for _ in range(6)]
+            print(f"{self.name} is still alive, dice pool is being reset.")
+            self.dice_pool.reset()
         else:
             print(f"{self.name} has died a gruesome death, you have lost. The world has ended.")
     
@@ -100,7 +95,8 @@ class Character:
 # needs overflow check method maybe?
 
 class Task:
-    def __init__(self, pattern):
+    def __init__(self, pattern:dict) -> None:
+        #needs validation that pattern is acceptable
         self.pattern = pattern
         self.slots = {key:0 for key in pattern.keys()}
 
@@ -121,11 +117,16 @@ class Task:
             self.slots[symbol] += number
         else:
             # should this be an error
-            raise ValueError(f"""{die_face} is not 
-            contained in the pattern for this task.""")
+            raise ValueError(f"{symbol} is not contained in the pattern for this task.")
         # what should I do about exceeding the maximum error?
         # maybe that is a separate method
     
+    # resets task for next attempt, if at all
+    # eventually this will be removed
+    def reset(self) -> None:
+        self.slots = {key:0 for key in self.pattern.keys()}
+
+
     @property
     def remaining(self) -> dict:
         return {key:max(0,self.pattern[key]-self.slots[key]) for key in self.pattern.keys()}
@@ -163,6 +164,9 @@ class TaskCard:
         string += f"reward: {self.reward}\n"
         string += f"penalty: {self.penalty}\n"
         return string
+
+    def __iter__(self):
+        return iter(self.tasks)
 
     @property
     def complete(self) -> bool:
@@ -222,6 +226,53 @@ class Die:
     def green(cls):
         return cls('1 x Investigate','2 x Investigate','3 x Investigate','1 x Scroll', '1 x Skull', '1 x Tentacles')
 
+class DicePool:
+    def __init__(self) -> None:
+        # implement the dunder method for this later
+        #for die in dice:
+        #    if type(die) != Die:
+        #        raise ValueError(f"Can not make a dice pool containing {die} since it is not a Die.")
+        self.dice = [Die.green() for _ in range(6)]
+    
+    # is this the correct thing
+    def __iter__(self) -> 'iterator':
+        return iter(self.dice)
+
+    #def __next__(self) -> 'Die':
+
+    def __getitem__(self,index) -> 'Die':
+        return self.dice[index]
+
+    def roll(self) -> None:
+        for die in self.dice:
+            die.roll()
+    
+    def pop(self, index:int=0) -> 'Die':
+        # defaults to removing first die
+        # this is because yellow and red dice will be appended and we 
+        # probably don't want to remove those by default
+        return self.dice.pop(index)
+    
+    def __str__(self) -> str:
+        return str(self.dice)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def __len__(self) -> int:
+        return len(self.dice)
+
+    def reset(self) -> None:
+        self.dice = [Die.green() for _ in range(6)]
+
+    
+
+    
+    # need methods for adding yellow and red dice
+
+def pause() -> None:
+    input("Hit enter to continue.")
+
 def assign_dice_from_pool_to_task(pool, task):
     """
     Assigns dice from pool to fixed task as long as it is possible.
@@ -229,7 +280,11 @@ def assign_dice_from_pool_to_task(pool, task):
     """
     # current pool is fixed
     if not pool in task:
+        print(pool)
+        print(task)
         print("None of the symbols of this roll are in this task.")
+        pause()
+        pool.pop()
         return pool, task
     while not task.complete and pool in task:
         print()
@@ -241,6 +296,8 @@ def assign_dice_from_pool_to_task(pool, task):
         # has 8 investiagte symbols.
         if index == "pass":
             print("Not attempting to assign any dice from this roll.")
+            print("Removing one die form the beginning of your dice pool.")
+            pool.pop(0)
             return pool, task
         die = pool[index-1]
         print(f"{index=} and {die=}.")
@@ -249,6 +306,7 @@ def assign_dice_from_pool_to_task(pool, task):
             task.assign_die(die)
         except ValueError as e:
             print(e)
+            pause
             continue
         else:
             pool.pop(index-1)
@@ -257,25 +315,57 @@ def assign_dice_from_pool_to_task(pool, task):
         return pool, task
     else:
         print("There are no longer any symbols you can use in this roll for this task.")
-        return pool.pop(0), task
+        print(pool)
+        print("Removing one die form the beginning of your dice pool.")
+        if len(pool)>0:
+            pool.pop(0)
+        return pool, task
 
-def attempt_task(character, task):
-    #character.roll_dice()
-    while character.dice > 0 and not task.complete:
-        print("starting while loop in attempt task")
-        print(character.dice_pool)
-        print(task)
+def attempt_task(dice_pool, task):
+    print(f"Attempting: {task}")
+    print(f"with {dice_pool}")
+    
+    #print("starting while loop in attempt task")
+    while len(dice_pool) > 0 and not task.complete:
         print("calling assign dice from pool to task function")
-        character.dice_pool, task = assign_dice_from_pool_to_task(character.dice_pool, task)
-        print("rerolling dice in pool")
-        character.roll_dice()
-   
+        dice_pool, task = assign_dice_from_pool_to_task(dice_pool, task)
+        if not task.complete:
+            print("rerolling dice in pool")
+            dice_pool.roll()
+            print(f"You rolled {dice_pool}")
+            print(f"{task.remaining} is still remaining.")
     if task.complete:
-        print(f'You have completed the task: {task.name}!')
+        print(f'You have completed this task!')
         #print(f"You get a {task.reward}.")
         return task
     else:
         print(f"You have failed, you die a tragic death")# the penalty of {task.penalty}.")
+        task.reset()
+
+def attempt_task_card(character:'Character',task_card:'TaskCard'):
+    # do we roll here? I think so.
+    character.roll_dice()
+    pool = character.dice_pool
+    print(pool)
+    for task in task_card:
+        print(task)
+    task_index = get_task_choice(len(task_card.tasks))
+
+
+def get_task_choice(num_tasks: int):
+    index = ""
+    valid_input = [str(num) for num in range(1,num_tasks+1)]
+    valid_input.append("pass")
+    while not index:
+        index = input(f"\nPlease input numbers 1-{num_tasks} to select which task to attempt.\n")
+        if index not in valid_input:
+            print(f"\n\n{index} is invalid.\n")
+            index = ""
+    if index.lower() == "pass":
+        return index.lower()
+    else:
+        return int(index)
+    
 
 def get_die_choice(num_dice: int): #return value for this is a bit complex
     # what about when num_dice = 0 or 1?
@@ -302,7 +392,8 @@ def create_generic():
     basic_task2 = Task(pattern={'Investigate':1, 'Scroll':2})#, reward="+2 health", penalty="-1 health")
     basic_old_one = GreatOldOne("basic old one", 10, (basic_task1, basic_task2), "+2 damage")
     basic_character = Character("joe shmoe",6)
-    return basic_old_one, basic_character
+    basic_card = TaskCard("basic task card", [basic_task1,basic_task2], "+1 damage", "-1 health")
+    return basic_old_one, basic_character, basic_card
 
 
 def start_game():
@@ -315,5 +406,7 @@ def start_game():
     pass
 
 # current_progress
-old_one, joe = create_generic()
+old_one, joe, sample_task_card= create_generic()
 c1, c2 = old_one.tasks
+c3 = Task({"Investigate":9})
+joe.roll_dice()
