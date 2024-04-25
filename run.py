31418aeb33e973fc4ee03c4536328c6162f939ba
Sean Tilson <seantilson@gmail.com>
2024-04-25 11:13:05 +0000

    in progress: refactor of attempt and assign functions improve their flow

--- run.py
+++ run.py
@@ -9,6 +9,20 @@ from game_pieces import *
 
 def pause() -> None:
     input("Hit enter to continue.")
+'''
+def assign_die_to_task(dice_pool, task): #'DicePool','Task':
+    index = get_die_choice(len(dice_pool))
+    if index == "pass":
+        dice_pool = pass_move(dice_pool)
+        return dice_pool, task
+    die = dice_pool[index-1]
+    if die in task:
+        task.assign_die(die)
+        dice_pool.pop(index-1)
+        pause()
+    else:
+        print(f"{str(die)} is not a valid choice for {task}.")
+'''
 
 # this function feels sloppy and not very streamlined/clean
 def assign_dice_to_task(dice_pool, task):
@@ -20,10 +34,9 @@ def assign_dice_to_task(dice_pool, task):
     """
     # This is still a bit messy with where the rolling and print statements happen being logically correct but slightly convoluted.
     # is the above still true?
-    while len(dice_pool) > 0:
+    # change to a do while loop, but how?
+    while not task.complete and len(dice_pool) > 0:
         # should this catch be at the beginning?
-        if task.complete:
-            return dice_pool, task
         report_dice_n_task(dice_pool, task)
         index = get_die_choice(len(dice_pool))
         if index == "pass":
@@ -39,12 +52,16 @@ def assign_dice_to_task(dice_pool, task):
     return dice_pool, task    
         
 def attempt_task(dice_pool, task):
-    # move this validation elsewhere
-    while len(dice_pool) > 0:
+    '''
+    Attmepts to complete task by assigning dice, and doing a pass move (lose die and reroll).
+    Returns dice pool and task if dice_pool is empty or task is complete.
+    '''
+    dice_pool, task = assign_dice_to_task(dice_pool, task)
+    while not task.complete and len(dice_pool) > 0:
         dice_pool, task = assign_dice_to_task(dice_pool, task)
-        if task.complete:
-            print('You have completed this task!')
-            return dice_pool, task
+    if task.complete:
+        print('You have completed this task!')
+        return dice_pool, task
     print("You are out of dice.")
     return dice_pool, task
 
@@ -60,23 +77,20 @@ def report_dice_n_task(dice_pool, task):
 
 def attempt_task_card(character:'Character',task_card:'TaskCard'):
     # do we roll here? I think so.
-    character.roll_dice()
     dice_pool = character.dice_pool
     #print(dice_pool)
     #for task in task_card:
      #   print(task)
     #I feel like I don't really need these conditions here
     #while not task_card.complete and len(dice_pool) > 0:
-    while True:
+    # change this to a do while loop, like in the get choice function
+    while not task_card.complete and len(dice_pool) > 0:
+        dice_pool.roll()
         report_options(dice_pool, task_card)
         task_index = get_task_choice(len(task_card.tasks))
         # Is this technically in the spirit of the game?
         if task_index == 'pass':
             dice_pool = pass_move(dice_pool)
-            # I think this print statement is irrelevant.
-            #print(dice_pool)
-            #for task in task_card:
-            #    print(task)
             continue
         task = task_card[task_index-1]
         # should this part of the validation be done elsewhere?
@@ -85,17 +99,13 @@ def attempt_task_card(character:'Character',task_card:'TaskCard'):
         else:
             print("Your roll doesn't have any symbols for that task.")
             continue
-        if len(dice_pool) == 0:
-            print(f"You have failed, you suffer the penalty of {task_card.penalty}.")
-            return task_card.penalty
-        elif task_card.complete:
-                print(f"You have completed {task_card.name}!")
-                print(f"You receive {task_card.reward}")
-                return task_card.reward
-        else:
-            dice_pool.roll()
-            
-
+    if task_card.complete:
+            print(f"You have completed {task_card.name}!")
+            print(f"You receive {task_card.reward}")
+            return task_card.reward
+    elif len(dice_pool) == 0:
+        print(f"You have failed, you suffer the penalty of {task_card.penalty}.")
+        return task_card.penalty
         
 # I don't like the reroll being part of this move, but I guess it is fine.
 def pass_move(dice_pool) -> 'DicePool':
